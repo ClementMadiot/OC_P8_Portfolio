7ec21e6d17963291299e90ed360dac619e450c90
ClementMadiot <clementmadiot09@gmail.com>
2023-10-08 14:37:21 +0200

    delete test files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
-import Test from './pages/Test';
 
 
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/test' element={<Test/>}/>
       <Route path='*' element={<Home />}/>
     </Routes>
     </BrowserRouter>
